7cecd2ad476f6a096f1578e12ef4456a35e6a86c
nguyendlp <nguyenduonghaui94@gmail.com>
2019-11-17 16:08:27 +0700

    commit section 2

--- s2/demo/src/main/java/com/example/demo/controller/HomeController.java
+++ s2/demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -2,14 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("home")
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
     public ModelAndView home() {
         return new ModelAndView("home");
     }
---
+++ s2/demo/src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+// su dung annotation @Data cua lombok
+@Data
+public class Student {
+    public Integer id;
+    public String name;
+    public int age;
+}
